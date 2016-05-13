67736cc39ee68e8a72f2ad7fd6839da271bcc925
Eric C. Anderson <eric.anderson@noaa.gov>
2016-05-12 17:06:25 -0700

    putting some genetics in there.

--- R-main/01-wiwa-analysis.R
+++ R-main/01-wiwa-analysis.R
@@ -6,11 +6,12 @@ library(gaiah)
 ####  Whether or not to re-compute things
 COMPUTE_ISO_POSTERIORS <- FALSE
 
+
+
+
 ####  Get the isotope posterior probs via Leave-one-out for the reference birds ####
 
-ref_birds <- breeding_wiwa_isotopes %>%
-  rename(ID = Field_Num)
-
+ref_birds <- breeding_wiwa_isotopes
 
 if(COMPUTE_ISO_POSTERIORS == TRUE) {
   isotope_ref_bird_results <- isotope_posterior_probs(isoscape = isomap_job54152_prediction,
@@ -23,3 +24,18 @@ if(COMPUTE_ISO_POSTERIORS == TRUE) {
 } else {
   load("outputs/isotope_ref_bird_results.rda")
 }
+
+# clip the isotope posteriors to the known breeding range and normalize again
+# I should rewrite the function to do this if we have the breeding range...
+isotope_ref_bird_results$loo_results <- lapply(isotope_ref_bird_results$loo_results, function(y) {
+  x <- y$posterior_probs
+  y$posterior_probs <- x * wiwa_breed / cellStats(x * wiwa_breed, sum)
+  y
+})
+
+
+
+#### Get the genetic posteriors and then make the genetic posterior rasters
+breeding_wiwa_genetic_posteriors
+
+
--- R/data.R
+++ R/data.R
@@ -11,13 +11,32 @@
 #' @format A tbl_df-ed (from dplyr) data frame with 357 rows and 14 variables. The relevant variables for
 #' analyses here are:
 #' \describe{
-#'   \item{Field_Num}{unique identifier for each bird}
+#'   \item{ID}{unique identifier for each bird}
 #'   \item{Isotope.Value}{hydrogen isotope ratios measured in the bird's feather}
 #'   \item{lat}{latitude of the bird's breeding/sampling location}
 #'   \item{long}{latitude of the bird's breeding/sampling location}
 #' }
 #' @source Kristen Ruegg, Jeff Kelly, Thomas Smith
 "breeding_wiwa_isotopes"
+
+
+
+#' Posterior probs of genetic region origin from Leave-one-out cross validation for breeding WIWAs
+#'
+#' A data frame of the same birds (roughly) that appear in \code{\link{breeding_wiwa_isotopes}}.  A long
+#' format data frame with 2,358 rows and 5 columns
+#'
+#' @format A tbl_df-ed (from dplyr) data frame with 357 rows and 14 variables. The relevant variables for
+#' analyses here are:
+#' \describe{
+#'   \item{ID}{unique identifier for each bird}
+#'   \item{Short_Name}{another id for the bird}
+#'   \item{Number of loci}{Number of loci successfully typed}
+#'   \item{region}{one of the genetic regions}
+#'   \item{posterior}{the posterior prob of originating from that region}
+#' }
+#' @source Kristen Ruegg, Eric Anderson, Thomas Smith
+"breeding_wiwa_genetic_posteriors"
 
 
 
@@ -58,3 +77,47 @@ get_wrld_simpl <- function() {
   load(system.file("data/wrld_simpl.rda", package = "maptools"))
   wrld_simpl
 }
+
+
+#' a raster of the breeding range of Wilson's warbler
+#'
+#' @format This a rasterized version of the breeding range of Wilson's warbler
+#' It contains 1's in the breeding range and 0's elsewhere.
+#' \describe{
+#' \item{class}{RasterLayer}
+#' \item{dimensions}{80, 228, 18240  (nrow, ncol, ncell)}
+#' \item{resolution}{0.5, 0.5  (x, y)}
+#' \item{extent}{-168.1, -54.1, 31.2, 71.2  (xmin, xmax, ymin, ymax)}
+#' \item{coord. ref.}{+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0}
+#' \item{data source}{in memory}
+#' \item{names}{layer}
+#' \item{values}{0, 1  (min, max)}
+#' }
+#'
+#' @source The rasters were generated from shapefiles provided to us by
+#' BirdLife International. (BirdLife International and NatureServe (2012)
+#' Bird species distribution maps of the world. BirdLife International, Cambridge,
+#' UK and NatureServe, Arlington, USA). Persons interested in the range map
+#' should contact BirdLife International http://www.birdlife.org/ or
+#' NatureServe http://www.natureserve.org/ directly.
+"wiwa_breed"
+
+
+
+
+#' raster brick showing the 6 genetic regions that Wilson's warblers may be assigned to
+#'
+#' @format RasterBrick. With 6 layers. Each contains 1's in the genetic region and 0's elsewhere.
+#' The sum of these layers is the raster \code{\link{wiwa_breed}}.
+#' \describe{
+#' \item{class}{ RasterBrick }
+#' \item{dimensions}{ 80, 228, 18240, 6  (nrow, ncol, ncell, nlayers)}
+#' \item{resolution}{ 0.5, 0.5  (x, y)}
+#' \item{extent}{ -168.1, -54.1, 31.2, 71.2  (xmin, xmax, ymin, ymax)}
+#' \item{coord. ref.}{ +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0 }
+#' \item{data source}{ Combine-Genetics/data/RegionBrick.grd }
+#' \item{names}{ CalSierra, Basin.Rockies, Eastern, AK.EastBC.AB, Wa.To.NorCalCoast, CentCalCoast }
+#' }
+#'
+#' @source Ruegg et al 2014
+"genetic_regions"
