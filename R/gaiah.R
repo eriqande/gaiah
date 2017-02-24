048d18abea667ea6d40344965e7d9cb4f23147b4
Eric C. Anderson <eric.anderson@noaa.gov>
2017-02-24 10:11:12 -0800

    Put link in to gaiah-wiwa

--- R/gaiah.R
+++ R/gaiah.R
@@ -6,8 +6,11 @@
 #' suitability data to calculate posterior probabilities of breeding origin of
 #' migrating birds.
 #'
-#' @section Section one:
-#' Put something here
+#' There is not a tutorial within the package, currently.  The best place to find an
+#' example of how to use the functions in this package is at the GitHub repository:
+#' \url{https://github.com/eriqande/gaiah-wiwa}. Go ahead and read the README there.
+#' It should provide you with everything you need to get up and running with
+#' the gaiah package.
 #'
 #' @docType package
 #' @name gaiah
